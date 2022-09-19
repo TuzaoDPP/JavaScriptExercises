e0b67bda51c88f7feaa369da202b762507f126e1
TuzaoDPP <arthenrique231@gmail.com>
2022-09-19 09:20:27 -0300

    Exercise10

---
+++ JavaExercise10/src/javaexercise10/JavaExercise10.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaexercise10;
+
+/**
+ *
+ * @author Gean
+ */
+public class JavaExercise10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ JavaExercise9/src/javaexercise9/JavaExercise9.java
@@ -0,0 +1,28 @@
+package javaexercise9;
+import java.util.*;
+
+public class JavaExercise9 {
+
+    
+    public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
+        
+        int numero;
+        
+        System.out.print("Digite um número: ");
+        numero = entrada.nextInt();
+        
+        System.out.println(numero + "x1 = " + numero * 1);
+        System.out.println(numero + "x2 = " + numero * 2);
+        System.out.println(numero + "x3 = " + numero * 3);
+        System.out.println(numero + "x4 = " + numero * 4);
+        System.out.println(numero + "x5 = " + numero * 5);
+        System.out.println(numero + "x6 = " + numero * 6);
+        System.out.println(numero + "x7 = " + numero * 7);
+        System.out.println(numero + "x8 = " + numero * 8);
+        System.out.println(numero + "x9 = " + numero * 9);
+        
+    }
+    
+}
