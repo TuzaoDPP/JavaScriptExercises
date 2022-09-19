ec85c147921a4149b2b42e168e54564af522cc8e
TuzaoDPP <arthenrique231@gmail.com>
2022-09-19 10:32:38 -0300

    Updated Exercise10

--- JavaExercise101/src/javaexercise101/JavaExercise101.java
+++ JavaExercise101/src/javaexercise101/JavaExercise101.java
@@ -17,6 +17,11 @@ public class JavaExercise101 {
         System.out.println("the successor of this number is: " + (number + 1));
         System.out.println("Twice that number is: " + (number * 2));
         System.out.println("Half of that number is: " + (number / 2));
+        System.out.println("This number squared  is: " + Math.pow(number, 2));
+        System.out.println("This number cubed is: " + Math.pow(number, 3));
+        System.out.format("The square root of that number is: %.2f \n" , Math.sqrt(number));
+        System.out.format("The cubic root of that number is: %.2f \n" , Math.cbrt(number));
+        
         
     }
     
