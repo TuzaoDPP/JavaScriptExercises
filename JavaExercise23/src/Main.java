6ed456b3e2b357436581fd0953d0bfd96ec20164
TuzaoDPP <arthenrique231@gmail.com>
2022-09-27 10:46:56 -0300

    Exercise23

---
+++ JavaExercise23/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        for(int i = 1; i <= 10; i++){
+            if(i % 2 == 0){
+                System.out.println(i);
+            }
+        }
+
+    }
+}
