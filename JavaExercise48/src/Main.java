b4d238279065df62dbf7df03f3edd7131038e5f5
TuzaoDPP <arthenrique231@gmail.com>
2022-10-19 09:56:19 -0300

    Exercise48

--- JavaExercise48/src/Main.java
+++ JavaExercise48/src/Main.java
@@ -11,17 +11,23 @@ public class Main {
         quantidade = input.nextInt();
 
         double[] valores = new double[quantidade];
-        double valor = 0;
 
         for(int i = 0; i < valores.length; i++){
-            System.out.format("Informe o valor do item %d: \n", i);
+            System.out.format("Informe o valor do item %d: \n", (i + 1));
             valores[i] = input.nextDouble();
-
-            valor += valores[i];
         }
-        System.out.format("O valor total gasto foi de R$ %.2f\n", valor);
-        System.out.format("A média dos valores gastos é de R$ %.2f\n", valor / quantidade);
-
+        System.out.format("O valor total gasto foi de R$ %.2f\n", valorTotal(valores));
+        System.out.format("A média dos valores gastos é de R$ %.2f\n", media(valores, quantidade));
     }
 
+    public static double valorTotal(double[] valores){
+        double valor = 0;
+        for(int i = 0; i < valores.length; i++){
+            valor += valores[i];
+        }
+        return valor;
+    }
+    public static double media(double[] valores, int quantidade){
+        return valorTotal(valores) / quantidade;
+    }
 }
