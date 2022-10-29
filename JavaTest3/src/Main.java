1ac26e51f5ab7b867d36967bbb930500b419b491
TuzaoDPP <arthenrique231@gmail.com>
2022-10-29 16:41:41 -0300

    UpdateExercise57

--- JavaTest3/src/Main.java
+++ JavaTest3/src/Main.java
@@ -2,15 +2,23 @@ import java.util.*;
 
 public class Main {
     public static double calculateDistance(double speed, double time){
-        return (speed * time) / 1000;
+        return speed * (time / 60);
     }
 
     public static double calculateSpeed(double distance, double time){
-        return (distance / time) * 1000;
+        return distance / (time / 60);
     }
 
     public static double calculateTime(double distance, double speed){
-        return (distance / speed) * 60;
+        return (distance / speed);
+    }
+
+    public static double convertHtoM(double distance, double speed){
+        return calculateTime(distance, speed) * 60;
+    }
+
+    public static double convertMtoH(double distance, double speed){
+        return calculateTime(distance, speed) / 60;
     }
 
     public static void main(String[] args) {
@@ -32,6 +40,7 @@ public class Main {
                 time = input.nextDouble();
 
                 System.out.println("The distance to be traveled is: " + calculateDistance(speed, time) + "km");
+                System.out.format("%.2f minutes converted to %.2f hour(s)\n", time, convertMtoH(calculateDistance(speed, time), speed));
                 break;
             case 2:
                 System.out.println("Inform the distance (in kilometers) to be traveled: ");
@@ -40,14 +49,15 @@ public class Main {
                 time = input.nextDouble();
 
                 System.out.format("The average speed of the travel is: %.2f km/h", calculateSpeed(distance, time));
+                System.out.format("%.2f minutes converted to %.2f hour(s)\n", time, convertMtoH(distance, calculateSpeed(distance, time)));
                 break;
             case 3:
-                System.out.println("Inform the distance to be traveled: ");
+                System.out.println("Inform the distance (in kilometers) to be traveled: ");
                 distance = input.nextDouble();
                 System.out.println("Inform the average speed (in kilometers per hour) of your vehicle: ");
                 speed = input.nextDouble();
 
-                System.out.format("The time you will be traveling is: %.2f hour(s)", calculateTime(distance, speed));
+                System.out.format("The time you will be traveling is: %.2f hour(s) or %.2f minutes", calculateTime(distance, speed), convertHtoM(distance, speed));
                 break;
             default: System.out.println("This option don't exist");
         }
